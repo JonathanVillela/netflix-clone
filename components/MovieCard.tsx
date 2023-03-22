b1e10ebe2c252ed1f0a256f4516decb7a2dd27db
JonathanVillela <villela.jonathan.m@gmail.com>
2023-03-22 15:07:40 -0500

    Versioning untracked files

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -2,7 +2,7 @@ import React, { useCallback } from 'react';
 import { useRouter } from 'next/router';
 import { BiChevronDown } from 'react-icons/bi'
 import { BsFillPlayFill } from 'react-icons/bs';
-import FavoriteButton from '@/components/FavoriteButton';
+import FavoriteButton from './FavoriteButton';
 import useInfoModal from '@/hooks/useInfoModel';
 
 interface MovieCardProps {
